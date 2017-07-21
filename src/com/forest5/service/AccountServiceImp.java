e7574982874d90b9ffacdf76c04efb83bcc6a92b
Forest Lettuce <azureforestryan@gmail.com>
2017-07-21 11:48:46 +0800

    spring事务管理的bug解决

--- src/com/forest5/service/AccountServiceImp.java
+++ src/com/forest5/service/AccountServiceImp.java
@@ -5,14 +5,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.forest5.dao.IAccountDao;
 
 public class AccountServiceImp implements IAccountService{
-	@Autowired
+	//@Autowired
 	private IAccountDao ad;
+	
+
+	public IAccountDao getAd() {
+		return ad;
+	}
+
+
+	public void setAd(IAccountDao ad) {
+		this.ad = ad;
+	}
+
 
 	@Override
 	public void transferMoney(String inname, String outname, double money) {
 		//从outname转出钱
 		ad.accountIn(inname, money);
-		//System.out.println(1/0);
+		System.out.println(1/0);
 		//转钱给inname
 		ad.accountOut(outname, money);
 	}
--- src/com/forest5/tests/AccountServiceImpTest.java
+++ src/com/forest5/tests/AccountServiceImpTest.java
@@ -7,15 +7,25 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.forest5.service.AccountServiceImp;
+import com.forest5.service.IAccountService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:applicationContext.xml")
 public class AccountServiceImpTest {
 	@Autowired
-	private AccountServiceImp service;
+	private IAccountService service;
+
+	/*public IAccountService getService() {
+		return service;
+	}
+
+	public void setService(IAccountService service) {
+		this.service = service;
+	}*/
 
 	@Test
 	public void testTransferMoney() {
+//		IAccountService service = new AccountServiceImp();
 		service.transferMoney("fox", "tom", 500);
 	}
 
